e7a8d6f098e713b4068b132215a123fde0e50c55
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-10-03 00:53:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,42 +3,42 @@ from textblob import TextBlob
 from googletrans import Translator
 from PIL import Image
 
+# Inicializar el traductor
 translator = Translator()
-st.title('Uso de TextBlob')
+st.title('Uso de textblob')
 
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 with st.sidebar:
     st.subheader("Polaridad y Subjetividad")
-    ("""
-    Polaridad: Indica si el sentimiento expresado en el texto es positivo, negativo o neutral. 
-    Su valor oscila entre -1 (muy negativo) y 1 (muy positivo), con 0 representando un sentimiento neutral.
+    st.markdown("""
+        Polaridad: Indica si el sentimiento expresado en el texto es positivo, negativo o neutral. 
+        Su valor oscila entre -1 (muy negativo) y 1 (muy positivo), con 0 representando un sentimiento neutral.
 
-    Subjetividad: Mide cuánto del contenido es subjetivo (opiniones, emociones, creencias) frente a objetivo
-    (hechos). Va de 0 a 1, donde 0 es completamente objetivo y 1 es completamente subjetivo.
+        Subjetividad: Mide cuánto del contenido es subjetivo (opiniones, emociones, creencias) frente a objetivo
+        (hechos). Va de 0 a 1, donde 0 es completamente objetivo y 1 es completamente subjetivo.
     """)
 
+# Análisis de polaridad y subjetividad
 with st.expander('Analizar Polaridad y Subjetividad en un texto'):
     text1 = st.text_area('Escribe por favor: ')
     if text1:
-        st.write('Texto ingresado: ', text1)  # Mostrar el texto ingresado
-
         blob = TextBlob(text1)
+        
+        # Mostrar polaridad y subjetividad
         polarity = round(blob.sentiment.polarity, 2)
         subjectivity = round(blob.sentiment.subjectivity, 2)
-
         st.write('Polaridad: ', polarity)
         st.write('Subjetividad: ', subjectivity)
-        
-        if polarity >= 0.5:
-            image = Image.open('Positivo.png')  
-            st.image(image, caption='Sentimiento Positivo 😊')
-        elif polarity <= -0.5:
-            image = Image.open('Negativo.png')  
-            st.image(image, caption='Sentimiento Negativo 😔')
-        else:
-            image = Image.open('Neutral.png')  
-            st.image(image, caption='Sentimiento Neutral 😐')
 
+        # Mostrar imagen según la polaridad
+        if polarity >= 0.5:
+            st.image('Positivo.png', caption='Sentimiento Positivo 😊', use_column_width=True)
+        elif polarity <= -0.5:
+            st.image('Negativo.png', caption='Sentimiento Negativo 😔', use_column_width=True)
+        else:
+            st.image('Neutral.png', caption='Sentimiento Neutral 😐', use_column_width=True)
+
+# Corrección en inglés
 with st.expander('Corrección en inglés'):
     text2 = st.text_area('Escribe por favor: ', key='4')
     if text2:
